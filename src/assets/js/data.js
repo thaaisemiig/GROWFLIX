1521911b09aa37f421c7e726e8548964210bdf50
Thaís Emig <thaisintelcell@gmail.com>
2024-08-27 19:25:34 -0300

    first commit

---
+++ src/assets/js/data.js
@@ -0,0 +1,8 @@
+const movies = [
+    {
+        img:'https://img.youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg',
+        title:'Growcast #00 - Conheça a Growdev',
+        link:'',
+        category:'growcast'
+    }
+]
